6fb93fb8e057984d5e91e4c2a6ec000c5621a9b3
AkaiKoinzell <96881015+AkaiKoinzell@users.noreply.github.com>
2025-06-18 12:05:44 +0200

    Create courses.js

---
+++ courses.js
@@ -0,0 +1,32 @@
+export const courses = {
+  "Golfclub Radstadt": {
+    tees: {
+      "Giallo": {
+        cr: 70.6,
+        slope: 127,
+        par: 72,
+        totalDistance: 5868,
+        holes: [
+          { number: 1, par: 4, hcp: 16, distance: 269 },
+          { number: 2, par: 4, hcp: 2, distance: 385 },
+          { number: 3, par: 4, hcp: 4, distance: 330 },
+          { number: 4, par: 5, hcp: 14, distance: 435 },
+          { number: 5, par: 4, hcp: 12, distance: 333 },
+          { number: 6, par: 3, hcp: 18, distance: 139 },
+          { number: 7, par: 5, hcp: 8, distance: 465 },
+          { number: 8, par: 3, hcp: 10, distance: 161 },
+          { number: 9, par: 5, hcp: 6, distance: 477 },
+          { number: 10, par: 3, hcp: 15, distance: 164 },
+          { number: 11, par: 5, hcp: 1, distance: 441 },
+          { number: 12, par: 3, hcp: 17, distance: 107 },
+          { number: 13, par: 4, hcp: 13, distance: 366 },
+          { number: 14, par: 4, hcp: 9, distance: 302 },
+          { number: 15, par: 4, hcp: 7, distance: 411 },
+          { number: 16, par: 4, hcp: 5, distance: 362 },
+          { number: 17, par: 5, hcp: 3, distance: 526 },
+          { number: 18, par: 3, hcp: 11, distance: 195 }
+        ]
+      }
+    }
+  }
+};
