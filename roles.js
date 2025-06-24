01d84614e92b1138321589524c8e593662141735
AkaiKoinzell <96881015+AkaiKoinzell@users.noreply.github.com>
2025-06-24 10:00:15 +0200

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,5 +1,5 @@
 // List of user email addresses allowed to access admin features
-export const ADMIN_EMAILS = ['admin@example.com'];
+export const ADMIN_EMAILS = ['l.m.devirgilio@gmail.com'];
 export function isAdmin(user) {
   return !!(user && ADMIN_EMAILS.includes(user.email));
 }
